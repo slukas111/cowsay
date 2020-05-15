7f5b60da44e0ac511af59589215d1e5e0039aeb2
Sasha Lukas <sashamati@gmail.com>
2020-05-15 09:50:03 -0700

    got the cow rendering, fixed issues in views and urls.

--- cowapp/urls.py
+++ cowapp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.urls import urlpatterns as cowapp_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
 ]
+urlpatterns += cowapp_urls
--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,8 @@ from myapp.forms import AddTextForm
 import subprocess
 # Create your views here.
 
+
+
 def index(req):
     if req.method == "POST":
         form = AddTextForm(req.POST)
@@ -11,7 +13,16 @@ def index(req):
         if form.is_valid():
             data = form.cleaned_data
             TextInput.objects.create(
-                text_input=data['add_text'])
-            
+                cowsay_text=data['text_input'])
+
+            out_put = subprocess.check_output(['cowsay', str(data['text_input'])]).decode('utf-8')
+
+
+            form = AddTextForm()
+            return render(req, 'index.html', {'form': form, 'out_put': out_put })
+
+    form = AddTextForm()
+    return render(req, 'index.html', {'form': form})
+
 
 
